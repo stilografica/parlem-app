fcd08b0d437375395399eb5d66a63120ab6339cc
Miri <miriamhinojo@stilografica.es>
2023-03-30 11:09:33 +0200

    Breacrumb component for CustomerDetail

---
+++ src/components/BreadcrumbBar.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { useRouter } from 'vue-router'
+
+  const props = defineProps({
+    items: Array,
+  });
+
+  const router = useRouter()
+</script>
+<template>
+  <v-breadcrumbs :items="items">
+    <template v-slot:divider>
+      <v-icon icon="mdi-chevron-right"></v-icon>
+    </template>
+  </v-breadcrumbs>
+</template>
+
+  
--- src/views/CustomerDetail.vue
+++ src/views/CustomerDetail.vue
@@ -1,9 +1,10 @@
 <script setup>
 import DefaultLayout from '@/components/layouts/DefaultLayout.vue'
 import CustomerDetailsCard from '@/components/CustomerDetailsCard.vue'
+import BreadcrumbBar from '@/components/BreadcrumbBar.vue'
 import CustomerProducts from '@/components/CustomerProducts.vue'
 import { useCustomersStore } from '@/stores/customers'
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
 import { useRoute } from 'vue-router'
 
 const props = defineProps({
@@ -12,9 +13,24 @@ const props = defineProps({
 const customersStore = useCustomersStore()
 const router = useRoute()
 const customer = computed(() => customersStore.getCustomer(router.params.id))
+
+const breadcrumbItems = ref([
+  {
+    title: 'Home',
+    href: '/'
+  },
+  {
+    title: 'Customers',
+    href: '/' // When the application grows this route will be /customers and we will have home
+  },
+  {
+    title: 'Customer detail',
+  },
+])
 </script>
 <template>
   <default-layout :has-items="true">
+    <breadcrumb-bar :items="breadcrumbItems"/>
     <v-row>
       <v-col cols="12" lg="5">
       <customer-details-card :customer="customer" />
